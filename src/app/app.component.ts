754156196d49791882de8bf343783e4f8f5708b4
Shreesh Yadav <shreeshyadav@gmail.com>
2025-04-11 19:18:34 -0700

    added starter project design

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -18,26 +18,28 @@ export class AppComponent {
 	@ViewChild(PortfolioComponent) portfolioComponent: PortfolioComponent | undefined;
 
 	clickedbutton: string = "";
+  isUserMenuOpen: boolean = false;
 	static buttonId: any;
   showwhatcomp: Number=1;
   constructor(){
 		this.button('srch');
 	}
 
-  button(buttonId: string) {
-		this.clickedbutton = buttonId;
-    this.showwhatcomp=1;
-		if (buttonId === 'srch') {
-      this.showwhatcomp=1;
-		} else if (buttonId === 'wchlst') {
-      this.showwhatcomp=2;
-      this.watchComponent?.ngOnInit();
-		} else if (buttonId === 'prtfl') {
-      this.showwhatcomp=3;
-      this.portfolioComponent?.ngOnInit();
-      //this.portfolioComponent?.constructor();
-		}
-	}
+  button(str: string) {
+    this.clickedbutton = str;
+    if (str === 'srch') {
+      this.showwhatcomp = 1;
+    }
+    else if (str === 'wchlst') {
+      this.showwhatcomp = 2;
+    }
+    else if (str === 'prtfl') {
+      this.showwhatcomp = 3;
+    }
+    else if (str === 'chat') {
+      this.showwhatcomp = 4;
+    }
+  }
 
   // Source : https://angular.io/api/core/EventEmitter
   // Source : https://stackoverflow.com/questions/46489081/emit-search-input-value-to-child-components-in-angular-2
@@ -48,5 +50,9 @@ export class AppComponent {
     this.searchComponent.setStock(tkr);
 		this.button('srch');
 	}
+
+  toggleUserMenu() {
+    this.isUserMenuOpen = !this.isUserMenuOpen;
+  }
 
 }
